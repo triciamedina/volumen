f7c359c3d0f60455712c11380d0b2e89eb56e840
triciamedina <tricia.medina@gmail.com>
2020-08-16 19:31:59 -0700

    Move some CSS to global stylesheet to handle TreeNode active and inactive states

--- src/components/TreeNode.js
+++ src/components/TreeNode.js
@@ -5,23 +5,24 @@ const TreeNode = (props) => {
     const { url, name, parent, type, color } = props;
 
     const styles = {
-        "root" : "text-2xl py-3 px-6",
-        "county" : "text-lg py-2 px-5",
-        "region" : "text-lg py-1 px-4"
+        "root" : "font-straight font-black text-2xl py-3 px-6",
+        "county" : "font-straight font-black text-lg py-2 px-5",
+        "region" : "font-straight font-black text-lg py-1 px-4"
     };
-
-    const active = { backgroundColor: `${color}`, marginBottom: "-4px"};
-    const inactive = { backgroundColor: `${color}`, opacity: .5, marginBottom: "-4px"};
+    
+    const opaque = { backgroundColor: `${color}`};
+    const transparent = { backgroundColor: `${color}4D`};
 
     return (
         <Match path={`${url}/*`}>
             {props =>
                 (
                     <Link 
-                        style={(props.match || props.location.pathname === parent) ? active : inactive} 
-                        className={`font-straight font-black border-gray-900 border-t-4 border-b-4 ${styles[type]}`} 
-                        to={url}>
-                            {name}
+                        to={url}
+                        style={(props.match || props.location.pathname === parent) ? opaque : transparent} 
+                        className={`${styles[type]} ${(props.match || props.location.pathname === parent) ? "active" : "inactive"}`} 
+                    >
+                        {name}
                     </Link>
                 )
             }
