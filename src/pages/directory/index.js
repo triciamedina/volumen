3a27389bbf3b5d63a8d63efa55b317bcff15d31c
triciamedina <tricia.medina@gmail.com>
2020-08-23 08:40:32 -0700

    Only show active column in mobile view

--- src/pages/directory/index.js
+++ src/pages/directory/index.js
@@ -22,55 +22,55 @@ export default class LibraryIndexPage extends React.Component {
                   {
                     title: "Select County",
                     matchPath: `/directory/*`,
+                    matchExact: `/directory`,
                     routes: ["/", "/:county", "/:county/:region", "/:county/:region/:neighborhood"],
                     type: "root"
                   },
                   {
                     title: "Select Region",
                     matchPath: `/directory/:county/*`,
+                    matchExact: `/directory/:county`,
                     routes: ["/:county", "/:county/:region", "/:county/:region/:neighborhood"],
                     type: "county"
                   },
                   {
                     title: "Select Neighborhood",
                     matchPath: `/directory/:county/:region/*`,
+                    matchExact: `/directory/:county/:region`,
                     routes: ["/:county/:region", "/:county/:region/:neighborhood/*"],
                     type: "region"
                   },
                   {
                     title: null,
                     matchPath: `/directory/:county/:region/*`,
+                    matchExact: `/directory/:county/:region/:neighborhood`,
                     routes: ["/:county/:region/:neighborhood"],
                     type: "neighborhood"
                   }
                 ].map((column, index) => (
 
-                  // Render column
-                  <div key={index} className={`flex-1 border-gray-900 ${column.type !== "neighborhood" && "border-r-4"}`}>
+                  <Match path={column.matchExact}>
+                    {props => (
 
-                    {/* Render title */}
-                    {column.title && 
-                      <Match path={column.matchPath}>
-                        {props => (
+                      <div key={index} className={`flex-1 border-gray-900 ${column.type !== "neighborhood" && "border-r-4"} ${!props.match && "hidden"} md:block`}>
+                        <Match path={column.matchPath}>
+                          {props => (
+                            column.title && 
                             <h2 className={`font-straight font-black text-2xl text-center py-3 ${props.match ? "text-gray-900": "text-gray-400"}`}>
                               {column.title}
                             </h2>
                           )}
-                      </Match>
-                    }
+                        </Match>
+                        <Router basepath="/directory">
+                          {column.routes.map((route, index) => <TreeView key={index} path={route} type={column.type} />)}
+                        </Router>
 
-                    {/* Render TreeView */}
-                    <Router basepath="/directory">
-                      {column.routes.map((route, index) => <TreeView key={index} path={route} type={column.type} />)}
-                    </Router>
-
-                  </div>
-
+                      </div>
+                    )}
+                  </Match>
                 ))
               }
-
             </div>
-
           </section>
         </div>
       </Layout>
